017bcbd3f207ed48296fca5097b8215c0d39c044
Alberto Rosado <albert.rosado@pm.me>
2023-07-29 14:24:07 -0700

    challenge solution

--- labs/memory_bound/data_packing/init.cpp
+++ labs/memory_bound/data_packing/init.cpp
@@ -2,12 +2,12 @@
 #include "solution.h"
 #include <random>
 
-S create_entry(int first_value, int second_value) {
-  S entry;
+S create_entry(short first_value, short second_value) {
+  S entry{};
 
   entry.i = first_value;
-  entry.s = static_cast<short>(second_value);
-  entry.l = static_cast<long long>(first_value * second_value);
+  entry.s = second_value;
+  entry.l = first_value * second_value;
   entry.d = static_cast<double>(first_value) / maxRandom;
   entry.b = first_value < second_value;
 
@@ -16,11 +16,11 @@ S create_entry(int first_value, int second_value) {
 
 void init(std::array<S, N> &arr) {
   std::default_random_engine generator;
-  std::uniform_int_distribution<int> distribution(minRandom, maxRandom - 1);
+  std::uniform_int_distribution<short> distribution(minRandom, maxRandom - 1);
 
-  for (int i = 0; i < N; i++) {
-    int random_int1 = distribution(generator);
-    int random_int2 = distribution(generator);
+  for (u_int16_t i = 0; i < N; i++) {
+    short random_int1 = distribution(generator);
+    short random_int2 = distribution(generator);
 
     arr[i] = create_entry(random_int1, random_int2);
   }
